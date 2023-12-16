47bbea8683f1f19386b73811ed4e79a8a5b92937
vincehsanchez <141890646+vincehsanchez@users.noreply.github.com>
2023-12-15 19:05:06 -0500

    so far so good

---
+++ Flask_Blog/flaskblog.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+import json
+from bson import json_util
+
+app = Flask(__name__, '/static') #flask needs to know where files are
+
+mongo = MongoClient(port = 27017)
+
+db = mongo.music
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/newyork")
+def newyork():
+    return render_template('newyork.html')
+
+@app.route("/losangeles")
+def losangeles():
+    return render_template('losangeles.html')
+
+@app.route("/atlanta")
+def atlanta():
+    return render_template('atlanta.html')
+
+@app.route("/dc")
+def dc():
+    return (render_template('dc1.html'))
+
+
+@app.route("/nashville")
+def nashville():
+    return render_template('nashville.html')
+
+@app.route("/austin")
+def austin():
+    return render_template('austin.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run() #do we need this?
